405a113d1d414cfbf74b774c0da65e5f4d7229de
Christian <christian.r.s.244@gmail.com>
2019-01-09 22:09:39 +0100

    Iteration 2 Done

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
-const urlBase = "http://exercise-1_messageapp_1:3000"
+const urlBase = "http://christian_sanz_messageapp_1:3000"
 
 const sendMessage = (destination, body) => {
     return axios.post(`${urlBase}/message`, { destination, body })
--- routes/routes.js
+++ routes/routes.js
@@ -4,25 +4,45 @@ const sendMessage = require("../client.js")
 
 
 router.get("/holamundo", (req, res, next) => {
-    res.status(200).json("Hola, mundo");
+    res.status(200).json("Hello, World");
 });
 
 router.post("/messages", (req, res, next) => {
     const { destination, message } = req.body
 
+    if (!Object.keys(req.body).includes("destination") || !Object.keys(req.body).includes("message")){
+        res.status(400).json("'destination' and 'message' parameters are requireds")
+        return
+    }
+
+    if (destination.length > 50 || message.length > 128){
+        res.status(413).json("destination respect 50 caracteres and message respect 128")
+        return
+    }
+
+    if (destination === "" || message === ""){
+        res.status(400).json("Destination and message parameters cant be empty")
+        return
+    }
+
+    if (typeof destination !== "string" || typeof message !== "string"){
+        res.status(400).json("destination and message parameters must be strings")
+        return
+    }
+
     sendMessage(destination, message)
         .then(response => {
             if (response.status === 200) {
-                res.status(200).json(response.data)
+                res.status(200).json(response.data + ", message sent")
                 return
             }
 
             if (response.status === 500) {
-                res.status(500).json(response.data)
+                res.status(500).json(response.data + ", Error to send the message")
                 return
             }
         })
-        .catch(err => res.status(500).json("Error al enviar el mensaje"))
+        .catch(err => res.status(500).json("Error to send the message"))
 });
 
 module.exports = router;
