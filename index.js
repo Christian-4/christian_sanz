f9cdd9a07d345ccbbd692d8d334974cd568f0f7e
Christian <christian.r.s.244@gmail.com>
2019-01-10 14:41:42 +0100

    Docker mongodb and connect my API with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const intervalID = setInterval(() => {
+    mongoose
+        .connect(process.env.DB, { useNewUrlParser: true })
+        .then(x => {
+            console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+            clearInterval(intervalID)
+        })
+        .catch(err => {
+            console.error('Error connecting to mongo', err)
+        })
+}, 1000)
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
