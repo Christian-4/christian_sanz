ee35ba30c697fe5bf6bb496e92ceedcaae38f812
Christian <christian.r.s.244@gmail.com>
2019-01-14 18:41:10 +0100

    testCredit Bug fixed

--- src/client/testCredit.js
+++ src/client/testCredit.js
@@ -4,7 +4,8 @@ const sendMessage = require("./sendMessage.js")
 module.exports = (res, destination, message, conditions = {}) => {
     Credit.find(conditions)
         .then(credits => {
-            if (credits[0].amount < 1) {
+            console.log(credits.length + " holaaaaa")
+            if (credits.length === 0 || credits[0].amount < 1) {
                 res.status(500).json("Insuficcient credit")
             } else {
                 sendMessage(res, destination, message, credits[0])
