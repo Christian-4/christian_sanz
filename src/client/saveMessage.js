5eccf529e9a6710ce3254f2696247e0f77645bfe
Christian <christian.r.s.244@gmail.com>
2019-01-16 11:47:52 +0100

    set saveMessage and sendMessage to replica

--- src/client/saveMessage.js
+++ src/client/saveMessage.js
@@ -1,10 +1,22 @@
-const {Message, Message2} = require("../models/Message");
+const { Message, Message2 } = require("../models/Message");
 
-module.exports = (destination, message, sent, confirmation) => {
-    return new Message({
-        destination,
-        message,
-        sent,
-        confirmation
-    }).save()
+module.exports = (destination, message, sent, confirmation, db) => {
+    if (db === "primary") {
+        return new Message({
+            destination,
+            message,
+            sent,
+            confirmation
+        }).save()
+    }
+
+    if (db === "replica") {
+        return new Message2({
+            destination,
+            message,
+            sent,
+            confirmation
+        }).save()
+    }
+
 }
